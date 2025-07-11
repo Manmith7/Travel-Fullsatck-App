95d1a305c75cc06439e662441bdacc9997ef233b
Manmith Gopari <119970011+Manmith7@users.noreply.github.com>
2025-07-12 00:25:58 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,12 @@ import HomeRoute from './routes/home.routes.js'
 import AuthRoutes from './routes/auth.routes.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
 const app = express();
 
 app.use(express.json())
-
+app.use(cors())
 connectDB();
 
 app.use('/',HomeRoute);
